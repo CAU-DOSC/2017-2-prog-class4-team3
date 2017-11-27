a118d2fa21e635bd1de75a7ec6347a78674cfd04
psangwoo <qkrtkddn130@naver.com>
2017-11-27 23:55:18 +0900

    Final

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 int main(void) 
 {
-	node *head = NULL;
-	head = (node *)malloc(sizeof(node));
+	int a;
 	int crt = 0;
 	int n;
-	int a;
-	while (!feof(stdin))
+	node *head = NULL;
+	head = (node *)malloc(sizeof(node));
+	while (!fflush(stdin))
 	{
 		if (crt == 0)
 			printf("Create %dst node (Ctrl + Z to terminate) >> ", crt + 1);
@@ -16,28 +16,29 @@ int main(void)
 		else
 			printf("Create %dth node (Ctrl + Z to terminate) >> ", crt + 1);
 		scanf("%d", &n);
-		appendnode(head, crt++, n); \
+		if (feof(stdin))
+			break;
+		appendnode(head, crt++, n);
 	}
-	while (crt-- > 0)
-	{
-		printf("PressNumber ( 1 : Delete, 2 : Print Value, 3 : Terminate ) >> ");
-		scanf("%d", &a);
-		if (a == 1)
-		{
-			int n2;
-			printf("Insert number to delete >> ");
-			scanf("%d", &n2);
-			deletenode(head, n2);
-		}
-		else if (a == 2)
-		{
-			int temp;
-			printf("Insert number to print >> ");
-			scanf("%d", &temp);
-			printf("Result : %d \n", printvalue(head, temp));
-		}
-		else if (a == 3)
-			exit(1);
-	}
+	printf("자료 개수 >> ");
+	printf("%d", countnode(head));
+	puts("");
+	printf("정순 출력 >> ");
+	printorder(head);
+	puts("");
+	printf("역순 출력 >> ");
+	printreverse(head);
+	puts("");
+	printf("중간값 출력 >> ");
+	printmid(head, countnode(head));
+	puts("");
+	printf("1 : 홀수번째 삭제, 2 : 짝수번째 삭제\n");
+	scanf("%d", &a);
+	printf("홀수번째 삭제 후 출력 >> ");
+	if(a==1)
+		delodd(head, countnode(head));
+	else if(a==2)
+		deleven(head, countnode(head));
+	puts("");
 	return 0;
 }
