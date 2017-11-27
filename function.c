b4663ca30ca7e194dc6ccce34b8c709bdf2cdad1
KimDaBin <ekqls6876@naver.com>
2017-11-27 20:14:02 +0900

    update function.c

--- function.c
+++ function.c
@@ -1,11 +1,11 @@
 #include "header.h"
 
-void appendnode(node *head, int crt, int value)
+void appendnode(node *head, int value)
 {
 	node *newnode = NULL;
 	newnode = (node *)malloc(sizeof(node));
 	newnode->value = value;
-	for (int i = 0; i < crt; i++)
+	while (head->next == NULL)
 		head = head->next;
 	head->next = newnode;
 }
@@ -24,4 +24,72 @@ int printvalue(node *head, int n)
 	for (int i = 0; i < n; i++)
 		crtnode = crtnode->next;
 	return crtnode->value;
+}
+int countnode(node *head)
+{
+	int i;
+	for (i = 1; head->next != NULL; i++)
+		head = head->next;
+	return i;
+}
+void printorder(node *head)
+{
+	if (head != NULL)
+	{
+		printf("%d ", head->value);
+		printorder(head->next);
+	}
+}
+void printreverse(node *head)
+{
+	if (head != NULL)
+	{
+		printreverse(head->next);
+		printf("%d ", head->value);
+	}
+}
+void printmid(node *head, int crt)
+{
+	int temp;
+	if (crt % 2 == 0)
+	{
+		temp = crt / 2;
+		for (int i = 1; i < temp; i++)
+			head = head->next;
+		printf("%d %d", head->value, head->next->value);
+	}
+	else
+	{
+		temp = (crt - 1) / 2;
+		for (int i = 0; i < temp; i++)
+			head = head->next;
+		printf("%d", head->value);
+	}
+}
+void delodd(node *head, int count)
+{
+	node *del = head;
+	head = head->next;
+	printf("%d ", head->value);
+	free(del);
+	for (int i = 0; i < (int)(count / 2) - 1; i++)
+	{
+		del = head->next;
+		head = head->next->next;
+		printf("%d ", head->value);
+		free(del);
+	}
+}
+void deleven(node *head, int count)
+{
+	node *del = head->next;
+	printf("%d ", head->value);
+	free(del);
+	for (int i = 0; i < (int)(count / 2); i++)
+	{
+		del = head->next;
+		head = head->next->next;
+		printf("%d ", head->value);
+		free(del);
+	}
 }
