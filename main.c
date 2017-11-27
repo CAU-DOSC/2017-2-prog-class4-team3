f6551c7a422f0744ec6a9ab5fa7c6402f6dba5e4
JunsukOH <33649873+JunsukOH@users.noreply.github.com>
2017-11-28 02:41:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -19,13 +19,16 @@ int main(void)
 	head = (node *)malloc(sizeof(node));
 	while (!fflush(stdin))
 	{
-		if (crt == 0) printf("Create %dst node (Ctrl + Z to terminate) >> ", crt + 1);
-		else if (crt == 1) printf("Create %dnd node (Ctrl + Z to terminate) >> ", crt + 1);
-		else printf("Create %dth node (Ctrl + Z to terminate) >> ", crt + 1);
+		if (crt == 0)
+			printf("Create %dst node (Ctrl + Z to terminate) >> ", crt + 1);
+		else if
+			(crt == 1) printf("Create %dnd node (Ctrl + Z to terminate) >> ", crt + 1);
+		else
+			printf("Create %dth node (Ctrl + Z to terminate) >> ", crt + 1);
 		scanf("%d", &value);
-		if (feof(stdin))
-			break;
+		if (feof(stdin)) break;
 		appendnode(head, crt++, value);
+		printf("\n");
 	}
 	printf("\n"); 
 	printf("자료들의 개수 >> ");
@@ -40,9 +43,21 @@ int main(void)
 	printf("중간값을 출력 >> ");
 	printmid(head, countnode(head));
 	printf("\n\n");
+	while (!fflush(stdin))
+	{
+		printf("원하는 순서의 노드 저장 값 (Ctrl + Z는 종료) >> ");
+		scanf("%d", &number);
+		printf("\n");
+		if (feof(stdin)) break;
+		if(0 < number && number <= countnode(head))
+			printf("%d번째 노드 저장 값 >> %d\n", number, printvalue(head, number));
+		else
+			printf("잘못된 값, 재 입력 필요\n");
+		printf("\n");
+	}
 	do
 	{
-		printf("1번(홀수번째 삭제), 2번(짝수번째 삭제) : ");
+		printf("1번(홀수번째 삭제), 2번(짝수번째 삭제) >> ");
 		scanf("%d", &number);
 		printf("\n"); 
 		if(number==1)
@@ -56,12 +71,9 @@ int main(void)
 			deleven(head, countnode(head));
 		}
 		else
-		{
-			printf("Error, Please re-enter.");
-			printf("\n");
-		}
+			printf("잘못된 값, 재 입력 필요\n");
 		printf("\n"); 
 	}while(number != 1 && number != 2);
-	
+
 	return 0;
 }
