b0831119f6a53fdbd744ff3fddb3f2099530e566
psangwoo <qkrtkddn130@naver.com>
2017-11-28 15:19:05 +0900

    Final

--- main.c
+++ main.c
@@ -1,16 +1,7 @@
 #include "header.h"
 
-void appendnode(node *head, int position, int value);
-void deletenode(node *head, int n);
-int printvalue(node *head, int n);
-int countnode(node *head);
-void printorder(node *head);
-void printreverse(node *head);
-void printmid(node *head, int count);
-void delodd(node *head, int count);
-void deleven(node *head, int count);
 
-int main(void) 
+int main(void)
 {
 	int number;
 	int crt = 0;
@@ -21,60 +12,43 @@ int main(void)
 	{
 		if (crt == 0)
 			printf("Create %dst node (Ctrl + Z to terminate) >> ", crt + 1);
-		else if
-			(crt == 1) printf("Create %dnd node (Ctrl + Z to terminate) >> ", crt + 1);
+		else if (crt == 1)
+			printf("Create %dnd node (Ctrl + Z to terminate) >> ", crt + 1);
 		else
 			printf("Create %dth node (Ctrl + Z to terminate) >> ", crt + 1);
 		scanf("%d", &value);
-		if (feof(stdin)) break;
+		if (feof(stdin))
+			break;
 		appendnode(head, crt++, value);
-		printf("\n");
 	}
-	printf("\n"); 
-	printf("자료들의 개수 >> ");
+	printf("\n");
+	printf("Number of Materials >> ");
 	printf("%d", countnode(head));
 	printf("\n\n");
-	printf("정순으로 출력 >> ");
+	printf("Print in Order >> ");
 	printorder(head);
 	printf("\n\n");
-	printf("역순으로 출력 >> ");
+	printf("Print in Reverse >> ");
 	printreverse(head);
 	printf("\n\n");
-	printf("중간값을 출력 >> ");
+	printf("Print Mid Value >> ");
 	printmid(head, countnode(head));
 	printf("\n\n");
-	while (!fflush(stdin))
+	printf("1 : Delete Materials in Odd Order , 2 : Delete Materials in Even Order >> ");
+	scanf("%d", &number);
+	printf("\n");
+	switch (number)
 	{
-		printf("원하는 차례의 노드 저장 값 (Ctrl + Z는 종료) >> ");
-		scanf("%d", &number);
-		printf("\n");
-		if (feof(stdin)) break;
-		if(0 < number && number <= countnode(head))
-			printf("%d번째 노드 저장 값 >> %d\n", number, printvalue(head, number));
-		else
-			printf("잘못된 값, 재 입력 필요\n");
-		printf("\n");
+	case 1:
+		printf("Print After Odd Delete >> ");
+		delodd(head, countnode(head));
+		break;
+	case 2:
+		printf("Print After Even Delete >> ");
+		deleven(head, countnode(head));
+		break;
+	default:
+		printf("Wrong Value \n");
 	}
-	do
-	{
-		printf("1번(홀수번째 삭제), 2번(짝수번째 삭제) >> ");
-		scanf("%d", &number);
-		printf("\n"); 
-		switch(number)
-		{
-			case 1 :
-				printf("홀수번째 삭제 후 출력 >> ");
-				delodd(head, countnode(head));
-				break;
-			case 2 :
-				printf("짝수번째 삭제 후 출력 >> ");
-				deleven(head, countnode(head));
-				break;
-			default :
-				printf("잘못된 값, 재 입력 필요\n");
-		}
-		printf("\n"); 
-	}while(number != 1 && number != 2);
-
 	return 0;
 }
